226ad0b4cbd58cd27c58fd66ffaef11d4a2817e6
axiom <axiom90@proton.me>
2024-04-20 14:13:21 +0300

    Update: expand/collapse output frame

--- desktop/run.py
+++ desktop/run.py
@@ -1,25 +1,42 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
 from PySide6.QtCore import QFile
 from ui.ui_hdwallet import Ui_MainWindow
 from widget.SvgButton import SvgButton
 
 
-class MyMainWindow(QMainWindow, Ui_MainWindow):
+class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.setWindowTitle("Hierarchical Deterministic Wallet")
 
         self.load_stylesheet("ui/css/dark-style.css")
 
         self.toggle_expand_terminal = SvgButton(
-            parent_widget=self.expandTerminalQFrame,
+            parent_widget=self.ui.expandTerminalQFrame,
             icon_path="ui/images/icon_maximize.svg",
             alt_icon_path="ui/images/icon_minimize.svg",
             icon_width=17,
             icon_height=17
         )
         self.toggle_expand_terminal.setCheckable(True)
+        self.toggle_expand_terminal.toggled.connect(self.toggle_expand)
+
+    def toggle_expand(self):
+        if self.toggle_expand_terminal.isChecked():
+            self.layout().removeWidget(self.ui.outputQFrame)
+            self.detached_window = QWidget()
+            self.detached_window.setWindowTitle("Detached Red Frame")
+            self.detached_window.resize(self.ui.outputQFrame.width(), self.ui.outputQFrame.height())
+            layout = QVBoxLayout()
+            layout.addWidget(self.ui.outputQFrame)
+            self.detached_window.setLayout(layout)
+            self.detached_window.show()
+            self.detached_window.setStyleSheet(self.styleSheet())
+        else:
+            self.detached_window.close()
+            self.ui.centralwidget.layout().addWidget(self.ui.outputQFrame)
 
     def load_stylesheet(self, path):
         style_file = QFile(path)
