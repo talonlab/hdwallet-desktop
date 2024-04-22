5ddd69f083d9345e61930f0a879a4e8f4dc14e5f
axiom <axiom90@proton.me>
2024-04-22 14:38:44 +0300

    Update: expand output frame fix

--- desktop/run.py
+++ desktop/run.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, Qt
 from ui.ui_hdwallet import Ui_MainWindow
 from widget.SvgButton import SvgButton
 
@@ -22,21 +22,39 @@ class MyMainWindow(QMainWindow):
         )
         self.toggle_expand_terminal.setCheckable(True)
         self.toggle_expand_terminal.toggled.connect(self.toggle_expand)
+        
+                
 
     def toggle_expand(self):
         if self.toggle_expand_terminal.isChecked():
+            self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint)     
+            self.showNormal()
+
             self.layout().removeWidget(self.ui.outputQFrame)
             self.detached_window = QWidget()
             self.detached_window.setWindowTitle("Detached Red Frame")
             self.detached_window.resize(self.ui.outputQFrame.width(), self.ui.outputQFrame.height())
+
             layout = QVBoxLayout()
             layout.addWidget(self.ui.outputQFrame)
+
             self.detached_window.setLayout(layout)
             self.detached_window.show()
             self.detached_window.setStyleSheet(self.styleSheet())
+            self.setMinimumWidth(0)
+
+            if not self.isMaximized():
+                self.resize(self.width() - self.ui.outputQFrame.width(), self.height())
+
         else:
             self.detached_window.close()
             self.ui.centralwidget.layout().addWidget(self.ui.outputQFrame)
+
+            if not self.isMaximized():
+                self.resize(self.width() + self.ui.outputQFrame.width(), self.height())
+                
+            self.setWindowFlags(self.windowFlags() | Qt.WindowMaximizeButtonHint)
+            self.show()
 
     def load_stylesheet(self, path):
         style_file = QFile(path)
