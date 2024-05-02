93afaa7c023f09a3b9755d3af95b2ecc3e8db38c
axiom <axiom90@proton.me>
2024-05-02 14:29:25 +0300

    Update: generate mnemonic and entropy

--- desktop/run.py
+++ desktop/run.py
@@ -1,7 +1,33 @@
 import subprocess
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, 
+    QPushButton, QVBoxLayout, QHBoxLayout, QFrame,
+    QStackedWidget
+)
 from PySide6.QtCore import QFile, Qt
+
+import string
+
+
+from hdwallet.entropies import (
+    AlgorandEntropy, ALGORAND_ENTROPY_STRENGTHS,
+    BIP39Entropy, BIP39_ENTROPY_STRENGTHS,
+    ElectrumV1Entropy, ELECTRUM_V1_ENTROPY_STRENGTHS,
+    ElectrumV2Entropy, ELECTRUM_V2_ENTROPY_STRENGTHS,
+    MoneroEntropy, MONERO_ENTROPY_STRENGTHS,
+    ENTROPIES
+)
+
+
+from hdwallet.mnemonics import (
+    AlgorandMnemonic, ALGORAND_MNEMONIC_WORDS, ALGORAND_MNEMONIC_LANGUAGES,
+    BIP39Mnemonic, BIP39_MNEMONIC_WORDS, BIP39_MNEMONIC_LANGUAGES,
+    ElectrumV1Mnemonic, ELECTRUM_V1_MNEMONIC_WORDS, ELECTRUM_V1_MNEMONIC_LANGUAGES,
+    ElectrumV2Mnemonic, ELECTRUM_V2_MNEMONIC_WORDS, ELECTRUM_V2_MNEMONIC_LANGUAGES, ELECTRUM_V2_MNEMONIC_TYPES,
+    MoneroMnemonic, MONERO_MNEMONIC_WORDS, MONERO_MNEMONIC_LANGUAGES,
+    MNEMONICS
+)
 
 from ui.ui_hdwallet import Ui_MainWindow
 from widget.SvgButton import SvgButton
@@ -20,6 +46,9 @@ class MyMainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.detached_window = None
+
+        # Setup UI Stuff
         self.setWindowTitle("Hierarchical Deterministic Wallet")
 
         self.load_stylesheet("ui/css/dark-style.css")
@@ -34,7 +63,82 @@ class MyMainWindow(QMainWindow):
         self.toggle_expand_terminal.setCheckable(True)
         self.toggle_expand_terminal.toggled.connect(self.toggle_expand)
         self.ui.outputTerminalQLineEdit.returnPressed.connect(self.process_command)
-        self.detached_window = None
+
+        self.ui.generateQPushButton.clicked.connect(lambda: self.change_page("hdwalletQStackedWidget", "generatePageQStackedWidget"))
+        self.ui.dumpQPushButton.clicked.connect(lambda: self.change_page("hdwalletQStackedWidget", "dumpsPageQStackedWidget"))
+
+        self._setup_generate_stack()
+
+    def _setup_generate_stack(self):
+        self.ui.generateEntropyClientQComboBox.addItems(ENTROPIES.names())
+        self.ui.generateEntropyClientQComboBox.currentIndexChanged.connect(self._generate_entropy_change)
+        self.ui.generateEntropyClientQComboBox.setCurrentText(BIP39Entropy.name())
+        self.ui.generateEntropyClientAndStrengthQPushButton.clicked.connect(self._generate_entropy)
+
+        self.ui.generateMnemonicClientQComboBox.addItems(MNEMONICS.names())
+        self.ui.generateMnemonicClientQComboBox.currentIndexChanged.connect(self._generate_mnemonic_change)
+        self.ui.generateMnemonicClientQComboBox.setCurrentText(BIP39Mnemonic.name())
+        self.ui.generateMnemonicClientWordsAndLanguageQPushButton.clicked.connect(self._generate_mnemonic)
+
+
+    def _generate_entropy_change(self):
+        entropy_client = self.ui.generateEntropyClientQComboBox.currentText()
+        self.ui.generateEntropyStrengthQComboBox.clear()
+        self.ui.generateEntropyStrengthQComboBox.addItems(
+            map(
+                str, ENTROPIES.entropy(entropy_client).strengths
+            )
+        )
+        self.ui.generateEntropyStrengthQComboBox.setCurrentIndex(0)
+
+    def _generate_entropy(self):
+        entropy_client = self.ui.generateEntropyClientQComboBox.currentText()
+        strength = int(self.ui.generateEntropyStrengthQComboBox.currentText())
+        gen_entropy = ENTROPIES.entropy(entropy_client).generate(strength=strength)
+        self.println(gen_entropy)
+
+
+    def _generate_mnemonic_change(self):
+        mnemonic_client = self.ui.generateMnemonicClientQComboBox.currentText()
+        self.ui.generateMnemonicWordsQComboBox.clear()
+        self.ui.generateMnemonicLanguageQComboBox.clear()
+
+        self.ui.generateMnemonicWordsQComboBox.addItems(
+            map(
+                str, MNEMONICS.mnemonic(mnemonic_client).words_list
+            )
+        )
+        self.ui.generateMnemonicLanguageQComboBox.addItems(MNEMONICS.mnemonic(mnemonic_client).languages)
+
+        self.ui.generateMnemonicWordsQComboBox.setCurrentIndex(0)
+        self.ui.generateMnemonicLanguageQComboBox.setCurrentText('english')
+
+
+    def _generate_mnemonic(self):
+        mnemonic_client = self.ui.generateMnemonicClientQComboBox.currentText()
+        word = self.ui.generateMnemonicWordsQComboBox.currentText()
+        lang = self.ui.generateMnemonicLanguageQComboBox.currentText()
+        gen_mnemonic = MNEMONICS.mnemonic(mnemonic_client).from_words(
+                            words=int(word),
+                            language=lang
+                        )
+
+        self.println(gen_mnemonic)
+
+    def change_page(self, stacked_name: str, widget_name : str) -> None:
+        qStackedWidget: Optional[QStackedWidget] = self.findChild(QStackedWidget, stacked_name)
+
+        if qStackedWidget is None:
+            print(f"QStackWidget not found: {stacked_name}")
+            return None
+
+        qWidget: Optional[QWidget] = qStackedWidget.findChild(QWidget, widget_name)
+
+        if qStackedWidget and qWidget: 
+            qStackedWidget.setCurrentWidget(qWidget)
+        else:
+            print(f"Error changing page: '{stacked_name}' '{widget_name}'")
+
 
     def process_command(self):
         result = subprocess.run(
@@ -45,9 +149,13 @@ class MyMainWindow(QMainWindow):
                 )
 
         output = result.stderr if result.stderr else result.stdout
-
         self.ui.outputTerminalQLineEdit.setText(None)
-        self.ui.outputTerminalQTextEdit.setText(output.decode())
+        self.println(output.decode())
+
+    def println(self, s):
+        #just for now
+        oldtext = self.ui.outputTerminalQTextEdit.toPlainText()
+        self.ui.outputTerminalQTextEdit.setText(oldtext + "\n" + str(s))        
 
     def toggle_expand(self):
         if self.toggle_expand_terminal.isChecked():
