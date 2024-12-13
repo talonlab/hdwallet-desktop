58005eaf61e7d574b938eaf5095b83c73b4789d5
itsm3abena <itsm3abena@gmail.com>
2024-12-13 12:54:30 +0300

    BUMP: hdwallet-desktop v0.1.0

--- src/info.py
+++ src/info.py
@@ -5,7 +5,7 @@
 # file COPYING or https://opensource.org/license/mit
 
 __name__: str = "hdwallet-desktop"
-__version__: str = "v0.1.0b13"
+__version__: str = "v0.1.0"
 __license__: str = "MIT"
 __author__: str = "Talon Lab"
 __email__: str = "info@talonlab.org"
